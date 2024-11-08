d6e7714a5a04b675031bb88955dc68c8161a8b42
ManojNambiraj <postboxmano@gmail.com>
2024-11-08 20:06:17 +0530

    JS Class 1

---
+++ JS/Class 1/script.js
@@ -0,0 +1,73 @@
+// Javascript - Functionalities
+
+// 1. Builtin funtions
+// 2. User-defined functions
+
+// alert("hi");
+// console.log("hi")
+
+// Datatypes:
+
+// Number
+
+// a = 123
+
+// console.log(typeof a, a);
+
+// // String
+
+// a = "xyz"
+
+// console.log(typeof a, a);
+
+// a = false
+
+// console.log(typeof a, a);
+
+// a = null
+
+// console.log(typeof a, a);
+
+// Variables:
+
+// my_name_is_character = 10;
+
+// console.log(my_name_is_character);
+
+// myNameIsCharacter = 300
+
+// console.log(myNameIsCharacter);
+
+
+// ES5 -> Ecma script 5
+
+// var
+
+// ES6 -> Ecma script 6
+
+// Let, const
+
+// // Var
+
+    // var a = 10;
+
+    // var a = 500;
+
+    // console.log(a);
+
+// // Let
+
+    // let b = 7
+
+    // b = 10;
+
+    // console.log(b);
+
+// // Const
+
+    const c = 500;
+
+    console.log(c);
+    
+    
+    
