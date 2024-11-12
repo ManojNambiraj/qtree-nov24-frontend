38631bf9f81f2408b66b747500f8c0bda5aa6f7a
ManojNambiraj <postboxmano@gmail.com>
2024-11-12 19:59:53 +0530

    JS Class 2 Operators

---
+++ JS/Class 2/script.js
@@ -0,0 +1,127 @@
+// Operators
+
+    // 1. Arithmetic Op --> (+, -, *, /, %, ** ++, --)
+
+    // let a = 5;
+    // let b = 2;
+
+    // let result;
+
+    // // result = a + b;
+    // // result = a - b;
+    // // result = a / b;
+    // // result = a * b;
+    // // result = a % b;
+    // result = a % b;
+    // result = a ** b;
+    // // result = ++a
+    // // result = --a
+
+    // console.log(result);
+
+    // 2. Assignment Op --> (=, +=, -=, *=, /=, %=, **=)
+
+    // let x = 3;
+
+    // // x += 5     // x = x + 5;
+    // // x -= 5     // x = x - 5;
+    // // x *= 5     // x = x * 5;
+    // // x /= 5     // x = x / 5;
+    // // x %= 5     // x = x % 5;
+    // // x *= 5     // x = x * 5;
+
+    // console.log(x);
+    
+    // 3. Comparision Op ---> (==, ===, >, >=, <, <=, !=, !==) --> Blooean
+
+    // let age = "10";
+    // let result;
+
+    // // result = (age == 10)
+    // // result = (age === 10)
+    // // result = (age > 10)
+    // // result = (age >= 10)
+    // // result = (age < 10)
+    // // result = (age <= 10)
+    // result = (age !== 10)
+
+    // console.log(result);
+
+    // 4. Logical Op ---> (&&, ||, !)
+
+    // &&
+
+        // True  && True  ----> True
+        // True  && False ----> False
+        // False && True  ----> False
+        // False && False ----> False
+
+    // ||
+
+        // True  || True  ----> True
+        // True  || False ----> True
+        // False || True  ----> True
+        // False || False ----> False
+
+    // !
+
+        // True  ----> false
+        // false ----> false
+
+        
+    // let z = 10;
+    // let result;
+
+    // result = (z == 10) && (z >= 10)
+    // result = (z != 10) || (z > 10)
+
+    // console.log(!result);
+
+    // 5. Bitwise Op ---> (&, |, ~, >>, <<)
+
+    // &
+
+        // 1  & 1  ----> 1
+        // 1  & 0  ----> 0
+        // 0  & 1  ----> 0
+        // 0  & 0  ----> 0
+
+    // |
+
+        // 1  | 1  ----> 1
+        // 1  | 0  ----> 1
+        // 0  | 1  ----> 1
+        // 0  | 0  ----> 0
+
+    // ~
+
+        // 1 ---> 0
+
+    // let a = 2;
+    // let result;
+
+    // // result = a & 3;
+    // result = a | 3;
+
+    // console.log(result);
+
+    // // 2 ---> 0010
+    // // 3 ---> 0011
+    // //       -------
+    // //        0011   --->  3
+
+    // 6. Ternary Op
+
+    let age = 15;
+
+    let result = (age > 18) ? "Eligible for voting" : "Not Eligible"
+
+    console.log(result);
+    
+
+    // 7. Typeof Op
+
+    // let a = "hi";
+
+    // console.log(typeof a);
+    
