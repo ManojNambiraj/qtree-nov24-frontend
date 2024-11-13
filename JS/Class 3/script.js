27c15b8d08d72099ea252215eb096f12ebd34abe
ManojNambiraj <postboxmano@gmail.com>
2024-11-13 20:12:10 +0530

    JS Conditional Stmts and Arrays objs

---
+++ JS/Class 3/script.js
@@ -0,0 +1,129 @@
+// Control Statements:
+
+    // 1. Conditional stmts
+            // --> if condt
+
+                // let age = "18";
+
+                // if(age >= 18){
+                //     console.log("Your Eligible for voting");
+                // }
+            
+            // --> if else
+
+                // if(age >= 18){
+                //     console.log("Your Eligible for voting");
+                // }else{
+                //     console.log("Not Eligible");
+                // }
+
+            // --> if else if
+
+                // if(age > 18){
+                //     console.log("Your Eligible for voting");
+                // }else if(age === 18){
+                //     console.log("Equal");
+                // }else if(age < 18){
+                //     console.log("Not Eligible");
+                // }else{
+                //     console.log("Something Went wrong");
+                // }
+
+            // --> switch case
+
+                // let day = "sun";
+
+                // switch(day){
+                //     case "Mon":
+                //         console.log("It's Monday");
+                //         break;
+                //     case "Tues":
+                //         console.log("It's Tuesday");
+                //         break;
+                //     case "Wed":
+                //         console.log("It's Wednesday");
+                //         break;
+                //     case "Thurs":
+                //         console.log("It's Thursday");
+                //         break;
+                //     case 5:
+                //         console.log("It's Friday");
+                //         break;
+                //     case 6:
+                //         console.log("It's Saturday");
+                //         break;
+                //     default:
+                //         console.log("Happy Weekend");
+                //         break;
+                // }
+
+    // 2. Looping Stmts
+
+        // 1. For loop
+
+            // for(stmt 1; stmt 2; stmt 4){
+                    // ...Stmt 3...
+            // }
+
+            // 1. stmt 1--> starting value (initialized value)
+            // 2. stmt 2--> conditional stmt
+            // 3. stmt 3--> executable stmt
+            // 4. stmt 4--> Incre / Decrement stmt
+
+            // for(let i = 1; i <= 10; i++){
+            //     console.log(i);
+            // }
+            
+        // 2. while loop
+
+            // let i = 5;
+
+            // while(i < 5){
+            //     console.log(i);
+            //     i++;
+            // }
+
+        // 3. do...while loop
+
+            // let i = 5;
+
+            // do{
+            //     console.log(i);
+            //     i++;
+            // }while(i < 5)
+
+// Array
+
+    let a = [10, 20, 30, 40, "hello", 60, true]
+
+    //      #0   #1  #2  #3..............
+
+    let len = a.length
+
+    console.log(a[len - 1]);
+    
+    // console.log(a[4]);
+
+// Object
+
+    // let obj = {
+    //     name: "Ravi", 
+    //     age: 22, 
+    //     dept: "IT", 
+    //     College: "XYZ",
+    //     favColor: ["black", "red", "blue"],
+    //     address: {
+    //         street: "1st",
+    //         landmark: "Watertank"
+    //     }
+    // }
+
+    // console.log(obj);
+    // console.log(obj.age);
+    // console.log(obj.favColor[1]);
+    // console.log(obj.address.landmark);
+    
+    
+    
+
+            
