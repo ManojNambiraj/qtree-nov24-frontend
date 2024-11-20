6acb09cd89c69265d5623f02eac97af31f4f4904
ManojNambiraj <postboxmano@gmail.com>
2024-11-20 20:29:57 +0530

    js class 5

---
+++ JS/Class 5/script.js
@@ -0,0 +1,68 @@
+// let fruits = ["apple", "banana", "Grapes", "kiwi", "Orange", "dragon"]
+
+// for(let i = 0; i < fruits.length; i++){
+//     console.log(fruits[i]);
+// }
+
+// Array methods: MRF (map, reduce, filter) 
+
+// fruits.map((item) => {
+//     console.log(item);
+// })
+
+// let num = [1, 5, 7, 2, 50, 49, 55, 97, 62]
+// let num = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+// let result = num.map((item) => {
+    
+//     return item * 2
+    
+//     // if(item % 2 != 0){
+//     //     return item * 100
+//     // }else{
+//     //     return item
+//     // }
+// })
+
+// console.log(result);
+
+// let result = num.filter((item) => {
+//     return (item % 2 == 0)
+// })
+
+// console.log(result);
+
+// let result = num.reduce((acc, curr) => {
+//     return acc + curr
+// })
+
+// console.log(result);
+
+// Destructuring:
+
+    // Array:
+
+    // let fruits = ["apple", "banana", "Grapes", "kiwi", "Orange", "dragon"]
+
+    // const [a, b, g, k, o, d] = fruits
+
+    // console.log(a);
+    // console.log(d);
+
+    // Object:
+
+    // let student = {
+    //     name: "ravi",
+    //     age: 23,
+    //     dept: "IT",
+    //     college: "xyz"
+    // }
+
+    // const {name, age, dept, college} = student
+
+    // console.log(dept);
+    
+    
+    
+    
+
