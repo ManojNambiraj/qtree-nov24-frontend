736376e151a52197e4e11a91007c311a513becaa
ManojNambiraj <postboxmano@gmail.com>
2024-11-21 20:37:12 +0530

    JS promise & Event listeners

---
+++ JS/Class 6/script.js
@@ -0,0 +1,55 @@
+// Promise
+
+// async function demo() {
+//   const myPromise = new Promise(function (resolve, reject) {
+//     let x = 50;
+
+//     if (x == 0) {
+//       resolve("Okay");
+//     } else {
+//       reject("Something went wrong");
+//     }
+//   });
+
+//   let result = await myPromise
+//     .then((data) => {
+//       return data;
+//     })
+//     .catch((err) => {
+//       return err;
+//     });
+
+//     console.log(result);
+// }
+
+// demo()
+
+
+// Event Listeners:
+
+    // onclick
+    // onchange
+
+    function handleChange(event) {
+        console.log(event.target.value);
+        
+    }
+
+    // let your_bank_balance = 1000
+
+    // const handleDeposite = (amount) => {
+    //     your_bank_balance += amount
+
+    //     console.log(your_bank_balance);
+    // }
+
+    // const handleWithdraw = (amount) => {
+    //     your_bank_balance -= amount
+
+    //     console.log(your_bank_balance);
+        
+    // }
+
+    
+    
+
